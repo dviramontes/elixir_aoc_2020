9f74c0039e67f758a213f28d3f7f2572befd720e
dviramontes <dviramontes@gmail.com>
2021-11-08 22:31:50 -0700

    solution day 15 / part 1

--- lib/advent_of_code/day_15/part_1.ex
+++ lib/advent_of_code/day_15/part_1.ex
@@ -1,41 +1,41 @@
 defmodule AdventOfCode.Day15.Part1 do
-  @moduledoc """
-  Solving Part 1 of Day 15
-  TODO: add description
-  """
-
-  # data structures
-
-  defmodule Recitations do
-    #    @typedoc """
-    #    A hashmap of recitations spoken
-    #    """
-    defstruct turn: 0, spoken: %{}, last: 0
-  end
-
   # public functions
 
   @doc """
-  Takes a input sequence and a number of runs to compute
+  Takes a input sequence and a number of rounds to compute
+  and returns the last number in the sequence
   """
   @spec run(list(integer), integer) :: integer
-  def run(sequence, runs = 2020) do
-    IO.inspect(init_state(sequence))
-    #    for i <- 1..runs do
-    #
-    #    end
+  def run(sequence, rounds = 2020) do
+    init_state(sequence)
+    |> Stream.unfold(&take_turn/1)
+    |> Stream.drop(rounds - length(sequence) - 1)
+    |> Enum.take(1)
+    |> hd()
+  end
+
+  @spec take_turn({integer, map(), integer}) :: {integer, {integer, map(), integer}}
+  def take_turn({turn, spoken, last}) do
+    case Map.get(spoken, last) do
+      nil ->
+        spoken = Map.put(spoken, last, turn)
+        {0, {turn + 1, spoken, 0}}
+
+      found ->
+        result = turn - found
+        spoken = Map.put(spoken, last, turn)
+        {result, {turn + 1, spoken, result}}
+    end
   end
 
   # private functions
 
-  def init_state(sequence) do
-    sequence
-    |> Enum.reduce(
-      %Recitations{},
-      fn num, %{turn: turn, spoken: spoken} ->
-        turn = turn + 1
-        %Recitations{turn: turn, spoken: Map.put(spoken, num, turn), last: num}
-      end
-    )
+  defp init_state(sequence) do
+    Enum.reduce(sequence, {0, %{}, 0}, &calc_state/2)
+  end
+
+  defp calc_state(num, {turn, spoken, _}) do
+    next_turn = turn + 1
+    {next_turn, Map.put(spoken, num, next_turn), num}
   end
 end
--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -6,8 +6,8 @@ defmodule AdventOfCodeTest.Day15Test do
   describe "day 15" do
     test "part 1" do
       assert Part1.run([1, 3, 2], 2020) == 1
-      #      assert Part1.run([2, 1, 3], 2020) == 10
-      #      assert Part1.run([0, 8, 15, 2, 12, 1, 4], 2020) == 123
+      assert Part1.run([2, 1, 3], 2020) == 10
+      assert Part1.run([0, 8, 15, 2, 12, 1, 4], 2020) == 289
     end
 
     test "part 2" do
